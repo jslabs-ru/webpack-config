a6799430cdda4f498916ce4cd11ec417eb47e527
designeng <denissavenok@gmail.com>
2021-02-08 14:46:43 +0300

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src', 'main.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.html')
+    })],
+};
